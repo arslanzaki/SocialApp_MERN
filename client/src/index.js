96350478f7c00a3e3093ee4da26181cf32a3f900
Arslan Zaki <59029560+arslanzaki@users.noreply.github.com>
2023-02-25 18:57:16 +0500

    MUI Theme Setup

--- client/src/index.js
+++ client/src/index.js
@@ -22,11 +22,12 @@ const persistConfig = { key: "root", storage, version: 1 };
 const persistedReducer = persistReducer(persistConfig, authReducer);
 const store = configureStore({
   reducer: persistedReducer,
-  middleware: (getDefaultMiddleware) => {
-    serializableCheck: {
-      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER];
-    }
-  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
